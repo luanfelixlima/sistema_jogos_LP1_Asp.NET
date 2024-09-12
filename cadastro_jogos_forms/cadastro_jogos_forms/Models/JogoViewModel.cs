72c7b0bacb902cf3dfd1b0d79c478e7194714786
luanfelixlima <luan.felix.lima@outlook.com>
2024-09-12 10:30:02 -0300

    Base do projeto do formulario

---
+++ cadastro_jogos_forms/cadastro_jogos_forms/Models/JogoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastro_jogos_forms.Models
+{
+    internal class JogoViewModel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double ValorLocacao { get; set; }
+        public int CategoriaId { get; set; }
+        public DateTime DataAquisicao { get; set; }
+    }
+}
